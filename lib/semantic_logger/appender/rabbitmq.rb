3f8b1f23296cdea79639e1a2e529d315ee2468e5
Fabio Pitino <pitinofabio@gmail.com>
2019-01-25 11:30:55 +0000

    add appender documentation

--- lib/semantic_logger/appender/rabbitmq.rb
+++ lib/semantic_logger/appender/rabbitmq.rb
@@ -28,11 +28,59 @@ end
 module SemanticLogger
   module Appender
     class Rabbitmq < SemanticLogger::Subscriber
-      # TODO: doc
+      # Create RabbitMQ appender using Bunny gem
       #
-      def initialize(queue_name: 'semantic_logger', rabbitmq_host: nil, metrics: true, **args, &block)
-        @queue_name = queue_name
-        @rabbitmq_args = args.merge(host: rabbitmq_host)
+      # Parameters:
+      #
+      #   queue_name: [String]
+      #     Name of RabbitMQ queue where to stream logs to.
+      #     This will be a queue bound to AMQP Default exchange
+      #     Default: semantic_logger
+      #
+      #   level: [:trace | :debug | :info | :warn | :error | :fatal]
+      #     Override the log level for this appender.
+      #     Default: SemanticLogger.default_level
+      #
+      #   formatter: [Object|Proc|Symbol|Hash]
+      #     An instance of a class that implements #call, or a Proc to be used to format
+      #     the output from this appender
+      #     Default: :json (See: #call)
+      #
+      #   filter: [Regexp|Proc]
+      #     RegExp: Only include log messages where the class name matches the supplied.
+      #     regular expression. All other messages will be ignored.
+      #     Proc: Only include log messages where the supplied Proc returns true
+      #           The Proc must return true or false.
+      #
+      #   host: [String]
+      #     Name of this host to appear in log messages.
+      #     Default: SemanticLogger.host
+      #
+      #   application: [String]
+      #     Name of this application to appear in log messages.
+      #     Default: SemanticLogger.application
+      #
+      # RabbitMQ Parameters:
+      #
+      #   rabbitmq_host: [String]
+      #     Host for AMQP connection. in Bunny this is called :host but here it has 
+      #     been remapped to avoid conflicting with SemanticLogger's :host param.
+      #     Default: localhost
+      #
+      #   username: [String]
+      #     Username for AMQP connection
+      #     Default: nil
+      #
+      #   password: [String]
+      #     Password for AMQP connection
+      #     Default: nil
+      #
+      #   more parameters supported by Bunny: http://rubybunny.info/articles/connecting.html
+      def initialize(queue_name: 'semantic_logger', rabbitmq_host: nil, metrics: false, **args, &block)
+        @queue_name             = queue_name
+        @rabbitmq_args          = args.dup
+        @rabbitmq_args[:host]   = rabbitmq_host
+        @rabbitmq_args[:logger] = logger
 
         super(level: level, formatter: formatter, filter: filter, application: application, host: host, metrics: metrics, &block)
         reopen
