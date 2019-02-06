3251384849e389c620736896ac35e5ac1bf634fd
Reid Morrison <reidmo@gmail.com>
2019-02-06 18:59:51 -0500

    Refine minitest log support

--- lib/semantic_logger/reporters/minitest.rb
+++ lib/semantic_logger/reporters/minitest.rb
@@ -4,26 +4,22 @@ module SemanticLogger
     # On completion the time it took to run the test is also logged.
     #
     # For example, add the following lines to `test_helper.rb`:
+    #   require 'minitest/reporters'
+    #
     #   reporters = [
     #     Minitest::Reporters::ProgressReporter.new,
     #     SemanticLogger::Reporters::Minitest.new
     #   ]
     #   Minitest::Reporters.use!(reporters)
     #
+    # And add `gem minitest-reporters` to the Gemfile.
+    #
     # Log entries similar to the following should show up in the log file:
     #
-    # 2019-01-30 14:41:21.590383 I [9989:70268303433760] (9.958ms) Minitest -- Passed: test_0002_infinite timeout
-    # 2019-01-30 14:41:21.590951 I [9989:70268303433760] Minitest -- Started: test_0002_must return the servers in the supplied order
-    # 2019-01-30 14:41:21.592012 I [9989:70268303433760] (1.019ms) Minitest -- Passed: test_0002_must return the servers in the supplied order
-    # 2019-01-30 14:41:21.592054 I [9989:70268303433760] Minitest -- Started: test_0003_must handle an empty list of servers
-    # 2019-01-30 14:41:21.592094 I [9989:70268303433760] (0.014ms) Minitest -- Passed: test_0003_must handle an empty list of servers
-    # 2019-01-30 14:41:21.592118 I [9989:70268303433760] Minitest -- Started: test_0001_must return one server, once
-    # 2019-01-30 14:41:21.592510 I [9989:70268303433760] (0.361ms) Minitest -- Passed: test_0001_must return one server, once
-    #
-    # Note:
-    # - To use `Minitest::Reporters::ProgressReporter` the gem `minitest-reporters` is required, as well as the
-    #   following line in `test_helper.rb`:
-    #     `require 'minitest/reporters'`
+    # 2019-02-06 18:58:17.522467 I [84730:70256441962000] Minitest -- START RocketJob::DirmonEntry::with valid entry::#archive_file test_0001_moves file to archive dir
+    # 2019-02-06 18:58:17.527492 I [84730:70256441962000] (4.980ms) Minitest -- PASS RocketJob::DirmonEntry::with valid entry::#archive_file test_0001_moves file to archive dir
+    # 2019-02-06 18:58:17.527835 I [84730:70256441962000] Minitest -- START RocketJob::DirmonEntry::#job_class::with a valid job_class_name test_0001_return job class
+    # 2019-02-06 18:58:17.529761 I [84730:70256441962000] (1.882ms) Minitest -- PASS RocketJob::DirmonEntry::#job_class::with a valid job_class_name test_0001_return job class
     class Minitest < ::Minitest::AbstractReporter
       include SemanticLogger::Loggable
 
@@ -32,16 +28,16 @@ module SemanticLogger
       attr_accessor :io
 
       def before_test(test)
-        logger.info('START', name: test.name)
+        logger.info("START #{test.class_name} #{test.name}")
       end
 
       def after_test(test)
         if test.error?
-          logger.benchmark_error('FAIL', payload: {name: test.name}, duration: test.time * 1_000, metric: 'minitest/fail')
+          logger.benchmark_error("FAIL #{test.class_name} #{test.name}", duration: test.time * 1_000, metric: 'minitest/fail')
         elsif test.skipped?
-          logger.benchmark_warn('SKIP', payload: {name: test.name}, duration: test.time * 1_000, metric: 'minitest/skip')
+          logger.benchmark_warn("SKIP #{test.class_name} #{test.name}", duration: test.time * 1_000, metric: 'minitest/skip')
         else
-          logger.benchmark_info('PASS', payload: {name: test.name}, duration: test.time * 1_000, metric: 'minitest/pass')
+          logger.benchmark_info("PASS #{test.class_name} #{test.name}", duration: test.time * 1_000, metric: 'minitest/pass')
         end
       end
     end
