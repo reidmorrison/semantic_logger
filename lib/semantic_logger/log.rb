c2f76e16f22274b4b81884f5306de4f35731fb33
Yoav Aner <yoav@gingerlime.com>
2020-04-19 13:10:05 +0000

    refs #146 restore SemanticLogger::Log.to_h

--- lib/semantic_logger/log.rb
+++ lib/semantic_logger/log.rb
@@ -281,6 +281,12 @@ module SemanticLogger
       !(payload.nil? || (payload.respond_to?(:empty?) && payload.empty?))
     end
 
+    def to_h(host = SemanticLogger.host, application = SemanticLogger.application, environment = SemanticLogger.environment)
+      logger = Struct.new(:host, :application, :environment)
+                     .new(host, application, environment)
+      SemanticLogger::Formatters::Raw.new.call(self, logger)
+    end
+
     # Lazy initializes the context hash and assigns a key value pair.
     def set_context(key, value)
       (self.context ||= {})[key] = value
