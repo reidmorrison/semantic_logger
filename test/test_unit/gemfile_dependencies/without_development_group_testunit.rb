e5957fc184b6ae98f6db8f01ce34927c9630f811
Todd A. Jacobs <385462+todd-a-jacobs@users.noreply.github.com>
2022-02-21 02:26:13 -0500

    validate that the gem works without excluded gems
    Use the Minitest gem as a canary to ensure that the :development gem
    group is not loaded, and that the gem works properly without the gems in
    that group.

---
+++ test/test_unit/gemfile_dependencies/without_development_group_testunit.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# Use Minitest as a canary gem to ensure that items moved to the :development
+# group aren't required by the rest of the library, and are only for development
+# or testing. As a result, it uses the Test::Unit library to ensure we're not
+# pulling in Minitest.
+
+# Allow pulling gems like Test::Unit from the Ruby Standard Library even if
+# running inside a bundle due to `bundle exec`.
+require "bundler"
+Bundler.reset!
+
+require "test/unit"
+
+Test::Unit.at_start do
+  # self.remove_const(MiniTest) if defined? MiniTest
+  # self.remove_const(Minitest) if defined? Minitest
+
+  # Pull in only the default gems.
+  Bundler.reset!
+  Bundler.require :default
+end
+
+# Ensure that we reset bundler so as not to interfere with the Minitest runners
+# or other require statements outside this individual suite of tests.
+Test::Unit.at_exit do
+  Bundler.reset!
+  Bundler.require :default, :development
+  raise unless defined?(MiniTest) && defined?(Minitest)
+end
+
+class WithoutDevelopmentGroupGems < Test::Unit::TestCase
+  def test_1_MiniTest_constant_is_undefined
+    refute defined?(MiniTest), "minitest loaded from :development group"
+  end
+
+  def test_2_Minitest_constant_is_undefined
+    refute defined?(Minitest), "minitest loaded from :development group"
+  end
+
+  def test_3_logger_succeeds_without_development_gems
+    msg = "test without :development gems"
+    logger = SemanticLogger::Test::CaptureLogEvents.new
+    logger.info msg
+    assert_equal msg, logger.instance_variable_get(:@events).pop.message
+  end
+end
